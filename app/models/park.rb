1c3251b84b4a739132c7c78c313808bcfbe24938
ladinya <lzhai2015@kellogg.northwestern.edu>
2015-02-16 23:46:01 -0600

    Update on 2.16

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,4 +2,6 @@ class Park < ActiveRecord::Base
   belongs_to :airport
   has_many :park_features
   has_many :features, through: :park_features
+  validates :name, presence: true
+  validates :country, presence: true
 end
